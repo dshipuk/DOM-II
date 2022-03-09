7f07335116d591d9f6d07f1306d8490d9ab90a38
unknown <dshipuk@yahoo.com>
2022-03-08 16:13:13 -0800

    added 5 events click, mouseover, wheel, load, keydown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// 1
+const btn = document.querySelectorAll(".btn")
+console.log(btn)
+function btAlert() {
+    alert ("Done")
+}
+btn.forEach(ele => {
+    ele.addEventListener("click", btAlert)
+})
+
+// 2
+const footer = document.querySelector("footer")
+function headerAlert() {
+    alert ("mouse over footer")
+}
+footer.addEventListener("mouseover", headerAlert)
+
+// 3
+const nav = document.querySelectorAll(".nav-link")
+
+nav.forEach(ele => {
+    ele.addEventListener("wheel", event => {
+        const delta = Math.sign(event.deltaY)
+        console.info(delta)
+    })
+})
+
+// 4
+window.addEventListener("load", (event) => {
+    console.log("page loaded")
+})
+
+// 5
+window.addEventListener("keydown", evt => {
+    console.log(evt)
+})
+
+// 6
